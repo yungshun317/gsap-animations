90a912f8cd4b294b86e98ef1604a49ab30aebc81
yungshun317 <seraph317@gmail.com>
2023-02-20 10:38:51 +0800

    [FEAT] Make comparison among `gsap.to`, `gsap.from`, and `gsap.fromTo`

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ gsap.to("#image", {
 });
 */
 
+/*
 gsap.to(".rect", {
     delay: 1,
     duration: 3,
@@ -18,4 +19,28 @@ gsap.to(".rect", {
     skewX: 45,
     fill: "yellow",
     ease: "power1.in"
+});
+*/
+
+gsap.to("#b", {
+    delay: 2,
+    duration: 3,
+    x: 700,
+    repeat: -1
+});
+
+gsap.from("#g", {
+    delay: 2,
+    duration: 3,
+    x: 700,
+    repeat: -1
+});
+
+gsap.fromTo("#r", {
+    x: 200,
+}, {
+    delay: 2,
+    duration: 3,
+    x: 700,
+    repeat: -1
 });
