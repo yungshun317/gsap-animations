b0bc15d77de320c933f1b6c40ffe2319b804d116
yungshun317 <seraph317@gmail.com>
2023-02-20 10:28:56 +0800

    [FEAT] Manipulate the properties of `gsap.to`

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 console.log(gsap.version);
+// 3.12.5
 
+/*
 gsap.to("#image", {
     delay: 1,
     x: 350,
     y: 250,
     scaleX: 4,
     scaleY: 4
-})
+});
+*/
+
+gsap.to(".rect", {
+    delay: 1,
+    duration: 3,
+    rotate: 2880,
+    skewX: 45,
+    fill: "yellow",
+    ease: "power1.in"
+});
